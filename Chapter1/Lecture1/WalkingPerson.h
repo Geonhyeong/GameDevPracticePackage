770438b7ca408e1fbc383c5bf03231e4f1325109
Geonhyeong <42195466+Geonhyeong@users.noreply.github.com>
2021-02-20 20:01:20 +0900

    implement 1.3_5 practice example
    implement repulsor beam and update code overall

--- Chapter1/Lecture1/WalkingPerson.h
+++ Chapter1/Lecture1/WalkingPerson.h
@@ -11,118 +11,211 @@ namespace jm
 	- make a Person class and use it to draw many people.
 	- make an Ironman and allow for him to shoot repulsor beam with his right hand
 	*/
-
-	class WalkingPerson
+	class Beam
 	{
 	public:
 		vec2 center = vec2(0.0f, 0.0f);
-		//float time = 0.0f;
+
+		void draw()
+		{
+			beginTransformation();
+			translate(center);
+			drawFilledBox(Colors::skyblue, 1.0f, 0.08f);
+			endTransformation();
+		}
+	};
+
+	class Person
+	{
+	public:
+		vec2 center = vec2(0.0f, 0.0f);
+		vec2 direction = vec2(1.0f, 0.0f);
+		Beam *beam = nullptr;
+
+		float time = 0.0f;
+		bool _stand = false;
 
 	public:
-		void draw(const float &time)
+		void drawFaceAndBody()
 		{
-			translate(center);
 			// gold face
 			beginTransformation();
-			translate(0.0f, 0.12f);
-			drawFilledCircle(Colors::gold, 0.08f);
-			translate(0.05f, 0.03f);
-			drawFilledCircle(Colors::white, 0.01f); // while eye
-
-			endTransformation();
-
-			// yellow left arm
-			beginTransformation();
-			rotate(sin(time * 5.0f) * -30.0f);					// animation!
-			scale(1.0f, 2.0f);
-			translate(0.0f, -0.1f);
-			drawFilledBox(Colors::yellow, 0.05f, 0.18f);
-			endTransformation();
-
-			// green left leg
-			beginTransformation();
-			translate(0.0f, -0.6f);
-			translate(0.0f, 0.2f);
-			rotate(sinf(time * 5.0f + 3.141592f) * -30.0f);	// animation!
-			translate(0.0f, -0.2f);
-			drawFilledBox(Colors::green, 0.1f, 0.4f);
+			{
+				translate(0.0f, 0.12f);
+				drawFilledCircle(Colors::gold, 0.08f);
+				translate(direction.x * 0.05f, 0.03f);
+				drawFilledCircle(Colors::white, 0.01f); // while eye
+			}
 			endTransformation();
 
 			// red body
 			beginTransformation();
-			scale(1.0f, 2.0f);
-			translate(0.0f, -0.1f);
-			drawFilledBox(Colors::red, 0.13f, 0.2f);
+			{
+				scale(1.0f, 2.0f);
+				translate(0.0f, -0.1f);
+				drawFilledBox(Colors::red, 0.13f, 0.2f);
+			}
 			endTransformation();
-
-			// yellow right arm
+		}
+		void drawLeftArm()
+		{
+			// yellow left arm
 			beginTransformation();
-			rotate(sin(time * 5.0f) * 30.0f);					// animation!
-			scale(1.0f, 2.0f);
-			translate(0.0f, -0.1f);
-			drawFilledBox(Colors::yellow, 0.05f, 0.18f);
+			{
+				if (_stand == false)
+					rotate(sin(time * 5.0f) * -30.0f);					// animation!
+				scale(1.0f, 2.0f);
+				translate(0.0f, -0.1f);
+				drawFilledBox(Colors::yellow, 0.05f, 0.18f);
+			}
 			endTransformation();
+		}
+		void drawRightArm(bool _beam)
+		{
+			// yellow right arm
 
+			if (_beam == true)
+			{
+				beginTransformation();
+				{
+					rotate(direction.x * 90.0f);
+					scale(1.0f, 2.0f);
+					translate(0.0f, -0.1f);
+					drawFilledBox(Colors::yellow, 0.05f, 0.18f);
+				}
+				endTransformation();
+				this->shoot();
+			}
+			else
+			{
+				if (beam != nullptr)
+				{
+					delete beam;
+					beam = nullptr;
+					std::cout << "beam is deleted" << std::endl;
+				}
+				beginTransformation();
+				{
+					if (_stand == false)
+						rotate(sin(time * 5.0f) * 30.0f);					// animation!
+					scale(1.0f, 2.0f);
+					translate(0.0f, -0.1f);
+					drawFilledBox(Colors::yellow, 0.05f, 0.18f);
+				}
+				endTransformation();
+			}
+		}
+		void drawLeftLeg() {
+			// green left leg
+			beginTransformation();
+			{
+				translate(0.0f, -0.6f);
+				translate(0.0f, 0.2f);
+				if (_stand == false)
+					rotate(sinf(time * 5.0f + 3.141592f) * -30.0f);	// animation!
+				translate(0.0f, -0.2f);
+				drawFilledBox(Colors::green, 0.1f, 0.4f);
+			}
+			endTransformation();
+		}
+		void drawRightLeg() {
 			// green right leg
 			beginTransformation();
-			translate(0.0f, -0.6f);
-			translate(0.0f, 0.2f);
-			rotate(sinf(time * 5.0f + 3.141592f) * 30.0f);	// animation!
-			translate(0.0f, -0.2f);
-			drawFilledBox(Colors::green, 0.1f, 0.4f);
+			{
+				translate(0.0f, -0.6f);
+				translate(0.0f, 0.2f);
+				if (_stand == false)
+					rotate(sinf(time * 5.0f + 3.141592f) * 30.0f);	// animation!
+				translate(0.0f, -0.2f);
+				drawFilledBox(Colors::green, 0.1f, 0.4f);
+			}
 			endTransformation();
-
-			//time += this->getTimeStep();
 		}
-
-		//void shoot()
-		//{
-		//	translate(center);
-		//	// gold face
-		//	beginTransformation();
-		//	translate(0.0f, 0.12f);
-		//	drawFilledCircle(Colors::gold, 0.08f);
-		//	translate(0.05f, 0.03f);
-		//	drawFilledCircle(Colors::white, 0.01f); // while eye
-
-		//	endTransformation();
-		//}
+		void move(const float &dt, const bool &_beam)
+		{
+			beginTransformation();
+			{
+				translate(center);
+				this->drawLeftArm();
+				this->drawLeftLeg();
+				this->drawFaceAndBody();
+				this->drawRightArm(_beam);
+				this->drawRightLeg();
+			}
+			endTransformation();
+			time += dt;
+		}
+		void shoot()
+		{
+			if (beam == nullptr)
+			{
+				beam = new Beam;
+				std::cout << "beam is constructed" << std::endl;
+			}
+			beam->center = this->center;
+			beam->center.x = this->direction.x * 1.0f;
+			beam->center.y = 0.0f;
+			beam->draw();
+		}
 	};
 
-	class WalkingPersonExample : public Game2D
+	class WalkingPerson : public Game2D
 	{
 	public:
-		std::vector<WalkingPerson> myPerson;
-		float time = 0.0f;
+		Person myPerson;
 
 	public:
-		WalkingPersonExample()
+		WalkingPerson()
 			: Game2D("This is my digital canvas!", 1024, 768, false, 2)
-		{}
+		{
+		}
 
-		~WalkingPersonExample()
+		~WalkingPerson()
 		{
 		}
 
 		void update() override
 		{
-			for (int i = 0; i < 3; i++)
+			// move person
+			if (isKeyPressed(GLFW_KEY_LEFT))
 			{
-				WalkingPerson person;
-				person.center += vec2((float)i * 0.3, (float)i * 0.3);
-				person.draw(time);
-				myPerson.push_back(person);
+				myPerson.center.x -= 0.5f * getTimeStep();
+				myPerson.direction.x = -1.0f;
+				myPerson._stand = false;
+			}
+			if (isKeyPressed(GLFW_KEY_RIGHT))
+			{
+				myPerson.center.x += 0.5f * getTimeStep();
+				myPerson.direction.x = 1.0f;
+				myPerson._stand = false;
+			}
+			if (isKeyPressed(GLFW_KEY_UP))
+			{
+				myPerson.center.y += 0.5f * getTimeStep();
+				myPerson._stand = false;
+			}
+			if (isKeyPressed(GLFW_KEY_DOWN))
+			{
+				myPerson.center.y -= 0.5f * getTimeStep();
+				myPerson._stand = false;
 			}
 
-			/*if (isKeyPressedAndReleased(GLFW_KEY_SPACE))
+			// stand person
+			if (!isKeyPressed(GLFW_KEY_RIGHT) && !isKeyPressed(GLFW_KEY_LEFT) && !isKeyPressed(GLFW_KEY_UP) && !isKeyPressed(GLFW_KEY_DOWN))
 			{
-				for (auto &person : myPerson)
-				{
-					person.shoot();
-				}
-			}*/
+				myPerson._stand = true;
+			}
 
-			time += this->getTimeStep();
+			// rendering
+			// shoot repulsor beam
+			if (isKeyPressed(GLFW_KEY_SPACE))
+			{
+				myPerson.move(this->getTimeStep(), true);
+			}
+			else
+			{
+				myPerson.move(this->getTimeStep(), false);
+			}
 		}
 	};
 }
