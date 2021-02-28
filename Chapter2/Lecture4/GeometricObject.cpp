67f56d2febd73f5650f43cc822f887fc3c576f5c
Geonhyeong <42195466+Geonhyeong@users.noreply.github.com>
2021-03-01 03:18:51 +0900

    implement 2.4_1 practice example
    use smart pointer(unique_ptr)

--- Chapter2/Lecture4/GeometricObject.cpp
+++ Chapter2/Lecture4/GeometricObject.cpp
@@ -1,4 +1,5 @@
 #include "GeometricObject.h"
+#include <memory>
 
 #include "Triangle.h"
 #include "Circle.h"
@@ -7,23 +8,23 @@
 
 namespace jm
 {
-	GeometricObject * GeometricObject::makeTriangle(const RGB & _color, const vec2 & _pos, const float & _size)
+	std::unique_ptr<GeometricObject> GeometricObject::makeTriangle(const RGB &_color, const vec2 &_pos, const float &_size)
 	{
-		return new Triangle(_color, _pos, _size);
-	}
-	
-	GeometricObject * GeometricObject::makeCircle(const RGB & _color, const vec2 & _pos, const float & _size)
-	{
-		return new Circle(_color, _pos, _size);
-	}
-	
-	GeometricObject * GeometricObject::makeBox(const RGB & _color, const vec2 & _pos, const float & _width, const float & _height)
-	{
-		return new Box(_color, _pos, _width, _height);
+		return std::make_unique<Triangle>(_color, _pos, _size);
 	}
 
-	GeometricObject * GeometricObject::makeStar(const RGB & _color, const vec2 & _pos, const float & _os, const float & _is)
+	std::unique_ptr<GeometricObject> GeometricObject::makeCircle(const RGB &_color, const vec2 &_pos, const float &_size)
 	{
-		return new Star(_color, _pos, _os, _is);
+		return std::make_unique<Circle>(_color, _pos, _size);
+	}
+
+	std::unique_ptr<GeometricObject> GeometricObject::makeBox(const RGB &_color, const vec2 &_pos, const float &_width, const float &_height)
+	{
+		return std::make_unique<Box>(_color, _pos, _width, _height);
+	}
+
+	std::unique_ptr<GeometricObject> GeometricObject::makeStar(const RGB &_color, const vec2 &_pos, const float &_os, const float &_is)
+	{
+		return std::make_unique<Star>(_color, _pos, _os, _is);
 	}
 }
--- Chapter2/Lecture4/GeometricObject.h
+++ Chapter2/Lecture4/GeometricObject.h
@@ -10,10 +10,10 @@ namespace jm
 		vec2 pos;
 		RGB  color;
 
-		//virtual ~GeometricObject()
-		//{}
+		virtual ~GeometricObject()
+		{}
 
-		void init(const RGB & _color, const vec2 & _pos)
+		void init(const RGB &_color, const vec2 &_pos)
 		{
 			color = _color;
 			pos = _pos;
@@ -30,5 +30,10 @@ namespace jm
 			}
 			endTransformation();
 		}
+
+		static std::unique_ptr<GeometricObject> makeTriangle(const RGB &_color, const vec2 &_pos, const float &_size);
+		static std::unique_ptr<GeometricObject> makeCircle(const RGB &_color, const vec2 &_pos, const float &_size);
+		static std::unique_ptr<GeometricObject> makeBox(const RGB &_color, const vec2 &_pos, const float &_width, const float &_height);
+		static std::unique_ptr<GeometricObject> makeStar(const RGB &_color, const vec2 &_pos, const float &_os, const float &_is);
 	};
 }
